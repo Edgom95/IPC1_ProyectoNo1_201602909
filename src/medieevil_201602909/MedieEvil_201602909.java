632eea25ba485b7eb03630cfe32d1cc0d24dd4be
edgom <edgom@LAPTOP-B73IU4UI>
2017-12-20 18:33:09 -0600

    Interfaz.

---
+++ src/medieevil_201602909/MedieEvil_201602909.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package medieevil_201602909;
+
+/**
+ *
+ * @author edgom
+ */
+public class MedieEvil_201602909
+{
+    public static String Player1;
+    public static String Player2;
+
+    public static void main(String[] args)
+    {
+    Inicio_201602909 bg = new Inicio_201602909();
+    bg.setVisible(true);
+    }
+    
+}
